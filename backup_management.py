2c980ec8841a5bbbe333a80ed2f98aa9dc7c21f5
mark <rens2588@gmail.com>
2022-08-04 20:03:17 +0300

    remove_old_copies changes

--- backup_management.py
+++ backup_management.py
@@ -31,6 +31,16 @@ class BackupManagement(object):
                 (':old' if time_delta > b_object.last_modified < time_delta else ':new')
             )
 
+    def show_instance_only(self):
+        object_summary_iterator = self.bucket.objects.all()
+        for b_object in object_summary_iterator:
+            if b_object.key.split(sep='/')[0] == INSTANCE_NAME:
+                time_delta = datetime.now(b_object.last_modified.tzinfo) - timedelta(days=30)
+                print(
+                    f'{b_object}:' + str(b_object.last_modified) +
+                    (':old' if time_delta > b_object.last_modified < time_delta else ':new')
+                )
+
     def upload_to_s3(
             self,
             upload_filename=UPLOAD_S3_FILENAME,
@@ -90,5 +100,6 @@ class BackupManagement(object):
         """
         object_summary_iterator = self.bucket.objects.all()
         for b_object in object_summary_iterator:
-            if self.is_old_object(b_object):
-                self.delete_from_s3(key=b_object.key)
+            if b_object.key.split(sep='/')[0] == INSTANCE_NAME:
+                if self.is_old_object(b_object):
+                    self.delete_from_s3(key=b_object.key)
